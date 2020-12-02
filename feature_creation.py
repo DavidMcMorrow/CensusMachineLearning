5ebbc9bce996e7f3e27643de4c97713832851bdc
Matthew <matthew.runswick@gmail.com>
2020-12-02 16:15:40 +0000

    created a basic model for a signle input feature

--- feature_creation.py
+++ feature_creation.py
@@ -23,7 +23,7 @@ for i in range(0, len(education)):
         education[i] = 1
         number_of_each_Class[0] +=1
     elif (education[i] == "read only"):
-        education[i] = 2
+        education[i] = 0
         number_of_each_Class[1] +=1
     elif (education[i] == "neither"):
         education[i] = 3
@@ -34,26 +34,75 @@ for i in range(0, len(education)):
 
 print("count", number_of_each_Class)
 
+education = education.astype('int')
+
 #tokenise each of the input fields
 from sklearn.feature_extraction.text import CountVectorizer
+import math as m
 vectorizer = CountVectorizer()
 
 surname_input = vectorizer.fit_transform(surname.astype('U'))
 forename_input = vectorizer.fit_transform(forename.astype('U'))
-family_ID_input = family_ID
 parish_input = vectorizer.fit_transform(parish.astype('U'))
 county_input = vectorizer.fit_transform(county.astype('U'))
-age_input = age
+age_input = vectorizer.fit_transform(age.astype('U'))
 sex_input = vectorizer.fit_transform(sex.astype('U'))
 relation_to_head_of_household_input = vectorizer.fit_transform(relation_to_head_of_household.astype('U'))
 marital_status = vectorizer.fit_transform(marital_status.astype('U'))
 occupation_input = vectorizer.fit_transform(occupation.astype('U'))
+family_ID_input = vectorizer.fit_transform(family_ID.astype('U')) #!!!!!!!!!!!!!!!!!!!!!!!not working properly for family ID (skipping certain values) !!!!!!!!!!!!
 
+# print("family id values\n",  family_ID_input)
 
-#test for if it worked
+print("family id", family_ID_input.shape)
+print("surname", surname_input.shape)
+print("forename", forename_input.shape)
+print("parish", parish_input.shape)
+print("county", county_input.shape)
+print("age", age_input.shape)
+print("sex", sex_input.shape)
+print("relation to head of household", relation_to_head_of_household_input.shape)
+print("marital status", marital_status.shape)
+print("occupation", occupation_input.shape)
+
+# test for if it worked
 # for i in range(0, len(surname)):
-#     if(forename[i] == "William"):
-#         print("input ", i, "   ", forename_input[i])
+#     if(family_ID[i] == 1):
+#         print("input ", i, "   ", family_ID_input[i])
+
+#create input vector 
+# X = []
+# for i in range(0, len(surname)):
+#     X.append([surname_input[i],forename_input[i],parish_input[i],county_input[i],age_input[i],sex_input[i],relation_to_head_of_household_input[i],marital_status[[i]],occupation_input[i]])
+# X = np.array(X, dtype=object) #!!!!!!!!!!!!! need to convert to array before training a model
+
+#array X is 7465 x 9
+
+#test model
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, roc_curve
+
+indices = np.arange(len(surname))
+training_data, test_data = train_test_split(indices, test_size=0.2)
+
+model = LogisticRegression()
+model.fit(surname_input[training_data], education[training_data])
+label_predictions = model.predict(surname_input[test_data])
+correct_predictions = 0
+increment = 0
+for test_datapoints in test_data:
+    if(label_predictions[increment] == education[test_datapoints]):
+        correct_predictions += 1
+    increment += 1
+
+accuracy = correct_predictions/ np.size(label_predictions)
+print("\n\nLogistic regression model metrics")
+print("accuracy", accuracy)
+LR_confusion_matrix = confusion_matrix(education[test_data], label_predictions)
+print("confusion Matrix")
+print(LR_confusion_matrix)
 
 
+# ----------------------------------------------------------------------------------------------------------
 #general notes, only 40 ppl not from antrim (makes field kinda usless)

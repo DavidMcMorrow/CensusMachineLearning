46e70081c38c0459f1ddf6fa927fc224ddb3ae5b
Matthew <matthew.runswick@gmail.com>
2020-12-03 23:09:22 +0000

    merged with scraper

--- feature_creation.py
+++ feature_creation.py
@@ -2,6 +2,35 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+import math as m
+
+def produce_input_feature(list_of_inputs):
+
+    #create a single list containing all words used as inputs
+    all_fields = []
+    for input in list_of_inputs:
+        all_fields.extend(input.astype('U'))
+
+    #tokenise each of the input fields
+    vectorizer = CountVectorizer()
+    vectorizer.fit(all_fields)
+
+    feature_inputs = []
+    i = 0
+    for input in list_of_inputs:
+        feature_inputs.append(vectorizer.transform(surname.astype('U')).toarray())
+        print(vectorizer.transform(surname.astype('U')).toarray().shape)
+        i += 1
+
+    X = []
+    for input in feature_inputs:
+        X.extend(input)
+
+    X = np.array(X)
+    print("shape of X", X.shape)
+
+    return X
 
 def generalisingSmallClasses(className, newValue, threshold):
     uniqueValues = list(set(className))
@@ -45,75 +74,13 @@ print("count", number_of_each_Class)
 
 #make the number of labels match the size of the flattened input
 temp = []
-for i in range(0, 8):
+for i in range(0, 9):
     temp.extend(labels)
 labels = np.array(temp)
+print("labels", len(labels))
 
-
-#---------------------------------------------------------------- begining of function --------------------------------------------------------------------
-#create a single list containing all words used as inputs
-all_fields = []
-all_fields.extend(surname.astype('U'))
-all_fields.extend(forename.astype('U'))
-all_fields.extend(family_ID.astype('U'))
-all_fields.extend(parish.astype('U'))
-all_fields.extend(age.astype('U'))
-all_fields.extend(sex.astype('U'))
-all_fields.extend(relation_to_head_of_household.astype('U'))
-all_fields.extend(marital_status.astype('U'))
-all_fields.extend(occupation.astype('U'))
-
-#tokenise each of the input fields
-from sklearn.feature_extraction.text import CountVectorizer
-import math as m
-vectorizer = CountVectorizer()
-vectorizer.fit(all_fields)
-
-surname_input = vectorizer.transform(surname.astype('U')).toarray()
-forename_input = vectorizer.transform(forename.astype('U')).toarray()
-parish_input = vectorizer.transform(parish.astype('U')).toarray()
-age_input = vectorizer.transform(age.astype('U')).toarray()
-sex_input = vectorizer.transform(sex.astype('U')).toarray()
-relation_to_head_of_household_input = vectorizer.transform(relation_to_head_of_household.astype('U')).toarray()
-marital_status_input = vectorizer.transform(marital_status.astype('U')).toarray()
-occupation_input = vectorizer.transform(occupation.astype('U')).toarray()
-family_ID_input = vectorizer.transform(family_ID.astype('U')).toarray() #!!!!!!!!!!!!!!!!!!!!!!!not working properly for family ID (skipping certain values) !!!!!!!!!!!!
-
-print("\nfamily id", family_ID_input.shape)
-print("surname", surname_input.shape)
-print("forename", forename_input.shape)
-print("parish", parish_input.shape)
-print("age", age_input.shape)
-print("sex", sex_input.shape)
-print("relation to head of household", relation_to_head_of_household_input.shape)
-print("marital status", marital_status_input.shape)
-print("occupation", occupation_input.shape)
-print("education", education.shape)
-print("\n\nlables", len(labels))
-# they are all of type class 'scipy.sparse.csr.csr_matrix'
-
-# test for if it worked
-# for i in range(0, len(surname)):
-#     if(family_ID[i] == 1):
-#         print("input ", i, "   ", family_ID_input[i])
-        
-# # create input vector
-X = []
-# for i in range(0, len(surname)):
-#     X.append([surname_input[i], forename_input[i], parish_input[i], age_input[i], sex_input[i], relation_to_head_of_household_input[i], marital_status_input[i], occupation_input[i]])
-X.extend(surname_input)
-X.extend(forename_input)
-X.extend(parish_input)
-X.extend(age_input)
-X.extend(sex_input)
-X.extend(relation_to_head_of_household_input)
-X.extend(marital_status_input)
-X.extend(occupation_input)
-
-X = np.array(X)
-print("shape of X", X.shape)
-#---------------------------------------------------------------- end of function --------------------------------------------------------------------
-# array X is 7465 x 9
+#generate feature vector (pass in a array for each of the raw data columns (should all be the same length))
+X = produce_input_feature([surname, forename, family_ID, parish, age, sex, relation_to_head_of_household, marital_status, occupation])
 
 # test model
 # from sklearn.linear_model import LogisticRegression
